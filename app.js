86dd3cbf9e70c143cf8274c3da25495ca769daf4
Nathan Morton <nathan.j.morton@gmail.com>
2018-10-26 22:44:40 -0700

    this

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const socket = require('socket.io')(http);
-const port = 3000;
+const express = require('express'),
+app = express(),
+http = require('http').Server(app),
+socket = require('socket.io')(http);
+port = 3000;
 
 app.use(express.static('public'));
 
@@ -14,7 +14,12 @@ socket.on('connection', (client) => {
   client.on('chat', (data) => {
     socket.emit('chat', data);
   });
+  // Handle typing event
+  client.on('typing', (data) => {
+    client.broadcast.emit('typing', data);
+  });
 });
+
 
 http.listen(port, () => {
   console.log(`listening on port ${port}`);
--- public/chat.js
+++ public/chat.js
@@ -1,13 +1,14 @@
 $(() => {
-  const socket = io();
-  const messages = document.querySelector('.messages');
-  const handle = document.querySelector('.handle');
-  const message = document.querySelector('.message');
+  const socket = io(),
+    messages = document.querySelector('.messages'),
+    handle = document.querySelector('.handle'),
+    message = document.querySelector('.message'),
+    feedback = document.querySelector('.feedback');
 
   $('form').submit(() => {
     socket.emit('chat', {
       message: $('.message').val(),
-      handle: $('.handle').val()
+      handle: handle.value
     });
     
     $('.message').val('');
@@ -18,13 +19,24 @@ $(() => {
   //   $('.messages').append($('<li>').text(data.message));
   // });
 
-  document.querySelector('.message').addEventListener('keypress', () => {
+  message.addEventListener('keypress', function(){
     socket.emit('typing', handle.value);
-  })
+})
 
+  // feedback.textContent = handle.value;
+
+  
+  // feedback.innerHTML = 'test';
+  
   socket.on('chat', (data) => {
+    feedback.innerHTML = '';
     messages.innerHTML += `<li><strong>handle:</strong> <em>${data.handle}</em> <br>
     <strong>message:</strong> <em>${data.message}</em> </li>`;
   })
+  
+  socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+  });
+
 
 });
